a6dfecd1f62898318c5c32d2cc8c5c81b534aeef
rafael <rafaelcmd@gmail.com>
2020-12-14 18:12:41 -0300

    Created unit tests

---
+++ src/test/java/one/digitalinnovation/personapi/utils/PersonUtils.java
@@ -0,0 +1,38 @@
+package one.digitalinnovation.personapi.utils;
+
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
+import one.digitalinnovation.personapi.entity.Person;
+
+import java.time.LocalDate;
+import java.util.Collections;
+
+public class PersonUtils {
+
+    private static final String FIRST_NAME = "Rafael";
+    private static final String LAST_NAME = "Costa";
+    private static final String CPF_NUMBER = "051.860.534-57";
+    private static final long PERSON_ID = 1L;
+    public static final LocalDate BIRTH_DATE = LocalDate.of(2010, 10, 1);
+
+    public static PersonDTO createFakeDTO() {
+        return PersonDTO.builder()
+                .firstName(FIRST_NAME)
+                .lastName(LAST_NAME)
+                .cpf(CPF_NUMBER)
+                .birthDate("27-04-1983")
+                .phones(Collections.singletonList(PhoneUtils.createFakeDTO()))
+                .build();
+    }
+
+    public static Person createFakeEntity() {
+        return Person.builder()
+                .id(PERSON_ID)
+                .firstName(FIRST_NAME)
+                .lastName(LAST_NAME)
+                .cpf(CPF_NUMBER)
+                .birthDate(BIRTH_DATE)
+                .phones(Collections.singletonList(PhoneUtils.createFakeEntity()))
+                .build();
+    }
+
+}
